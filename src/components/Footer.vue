202059ebdbfb51ef4024062c06f336f834d15fdf
Robyn Carnow <robynainsley2i@gmail.com>
2024-08-29 15:00:15 +0200

    Edit About Page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,24 @@
 <template>
-  <div class="d-flex justify-content-center align-items-center sticky-bottom" id="footer">
-    <div class="col-md-6 d-flex justify-content-center align-items-center">
-      <img
-        class=" "
-        src="https://robynainsley21.github.io/images/capstone/FitMatch-logo-removebg-preview.png"
-        alt="footer-logo"
-        loading="lazy"
-      />
+  <div id="footer">
+    <div class="d-flex justify-content-center align-items-center" id="footer-items">
+      <div class="col-md-6 d-flex justify-content-center align-items-center">
+        <img
+          src="https://robynainsley21.github.io/images/capstone/FitMatch-logo-removebg-preview.png"
+          alt="footer-logo"
+          loading="lazy"
+        />
+      </div>
+      <div class="socials-box col-md-6">
+        <ul>
+          <li><i class="bi bi-telephone-fill"></i><span>012 345 6789</span></li>
+          <li>
+            <i class="bi bi-envelope-fill"></i><span>fitmatchTM@gmail.com</span>
+          </li>
+          <li><i class="bi bi-twitter-x"></i><span>@fitmatchTM</span></li>
+        </ul>
+      </div>
     </div>
-    <div class="socials-box col-md-6">
-      <ul>
-        <li><i class="bi bi-telephone-fill"></i><span>012 345 6789</span></li>
-        <li><i class="bi bi-envelope-fill"></i><span>fitmatchTM@gmail.com</span></li>
-        <li><i class="bi bi-twitter-x"></i><span>@fitmatchTM</span></li>
-      </ul>
-    </div>
+    <p class="text-center">&copy; Copyright `${currentYear}`</p>
   </div>
 </template>
 
@@ -29,7 +33,15 @@ export default {
   /* background-color: #fffbfe; */
   backdrop-filter: blur(10px);
   z-index: 1;
+  position: sticky;
+  display: flex;
+  flex-direction: column;
+
+  &:last-child {
+    color: #9b3922;
+  }
 }
+
 img[alt="footer-logo"] {
   width: 8rem;
 }
@@ -43,14 +55,20 @@ ul li {
 
   & i {
     font-size: 1rem;
-    margin-right: .5rem;
+    margin-right: 0.5rem;
   }
 }
 
 @media screen and (max-width: 425px) {
-    #footer{
-        display: flex;
-        flex-direction: column;
-    }
+  #footer {
+    display: flex;
+    flex-direction: column;
+  }
+
+  #footer-items{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
 }
 </style>
