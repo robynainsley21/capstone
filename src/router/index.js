eca9c1b9ae43d39f5ba0836918a785029a83864f
Robyn Carnow <robynainsley2i@gmail.com>
2024-09-03 14:54:06 +0200

    Login and SignUp page

--- src/router/index.js
+++ src/router/index.js
@@ -1,57 +1,88 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: () => import(/* webpackChunkName: "home" */ '../views/HomeView.vue')
+    path: "/",
+    name: "home",
+    component: () =>
+      import(/* webpackChunkName: "home" */ "../views/HomeView.vue"),
+    meta: { requiresAuth: true },
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: "/about",
+    name: "about",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
   {
-    path: '/trainers',
-    name: 'trainers',
-    component: () => import(/* webpackChunkName: "allTrainers" */ '../views/TrainersView.vue')
+    path: "/trainers",
+    name: "trainers",
+    component: () =>
+      import(/* webpackChunkName: "allTrainers" */ "../views/TrainersView.vue"),
+    meta: { requiresAuth: true },
+  },
+  
+  {
+    path: "/trainersSingleView/:id",
+    name: "trainersSingleView",
+    component: () =>
+      import(
+        /* webpackChunkName: "trainersSingleView" */ "../views/TrainerSingleView.vue"
+      ),
   },
   {
-    path: '/trainersSingleView/:id',
-    name: 'trainersSingleView',
-    component: () => import(/* webpackChunkName: "trainersSingleView" */ '../views/TrainerSingleView.vue')
+    path: "/checkout",
+    name: "checkout",
+    component: () =>
+      import(/* webpackChunkName: "checkout" */ "../views/CheckoutView.vue"),
+    meta: { requiresAuth: true },
   },
   {
-    path: '/checkout',
-    name: 'checkout',
-    component: () => import(/* webpackChunkName: "checkout" */ '../views/CheckoutView.vue')
+    path: "/contact",
+    name: "contact",
+    component: () =>
+      import(/* webpackChunkName: "contact" */ "../views/ContactView.vue"),
   },
   {
-    path: '/contact',
-    name: 'contact',
-    component: () => import(/* webpackChunkName: "contact" */ '../views/ContactView.vue')
+    path: "/admin",
+    name: "admin",
+    component: () =>
+      import(/* webpackChunkName: "admin" */ "../views/AdminView.vue"),
+    meta: { requiresAuth: true, requiresAdmin: true },
   },
   {
-    path: '/admin',
-    name: 'admin',
-    component: () => import(/* webpackChunkName: "admin" */ '../views/AdminView.vue')
+    path: "/login",
+    name: "login",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/LoginView.vue"),
   },
   {
-    path: '/login',
-    name: 'login',
-    component: () => import(/* webpackChunkName: "login" */ '../views/LoginView.vue')
+    path: "/signup",
+    name: "signup",
+    component: () =>
+      import(/* webpackChunkName: "signup" */ "../views/SignupView.vue"),
   },
-  {
-    path: '/signup',
-    name: 'signup',
-    component: () => import(/* webpackChunkName: "signup" */ '../views/SignupView.vue')
-  },
-
-]
+];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+  const requiresAdmin = to.matched.some(record => record.meta.requiresAdmin);
+
+  const userToken = localStorage.getItem("token");
+  const userRole = localStorage.getItem('role');
+
+  if (requiresAuth && !userToken) {
+    next({ name: "login" });
+  } else if (requiresAdmin && userRole !== 'admin') {
+    next({ name: "home" });
+  } else {
+    next();
+  }
+});
+
+export default router;
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,15 +1,100 @@
 <template>
-    <div>
-login view
-    </div>
+  <div class="login-container">
+    <h2 class="text-center">Login</h2>
+    
+    <form @submit.prevent="handleLogin">
+      <div class="form-group">
+        <label for="emailAdd">Email:</label>
+        <input type="email" id="emailAdd" v-model="emailAdd" required />
+      </div>
+      <div class="form-group">
+        <label for="userPass">Password:</label>
+        <input type="password" id="userPass" v-model="userPass" required />
+      </div>
+      <div class="form-group">
+        <label for="userRole">Role:</label>
+        <select id="userRole" v-model="userRole" required>
+          <option value="user">User</option>
+          <option value="admin">Admin</option>
+        </select>
+      </div>
+     <div class="button-container">
+        <button class="login-btn" type="submit">Login</button>
+        <router-link to="/signup"><button class="login-btn">Signup</button></router-link>
+     </div>
+    </form>
+
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "LoginView"
-    }
+export default {
+  name: "LoginView",
+  data() {
+    return {
+      emailAdd: "",
+      userPass: "",
+      userRole: "user",
+    };
+  },
+  methods: {
+    handleLogin() {
+      const payload = {
+        emailAdd: this.emailAdd,
+        userPass: this.userPass,
+        userRole: this.userRole,
+      };
+      this.$store.dispatch("loginUser", payload);
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.button-container{
+    display: flex;
+    justify-content: space-between;
+}
+.login-container {
+  margin: 0 auto;
+  padding: 20px;
+  background: #fffbfe;
+  border-radius: 8px;
+}
 
+form{
+    width: 400px;
+    margin: 0 auto;
+}
+
+.form-group {
+  margin-bottom: 15px;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+input {
+  width: 100%;
+  padding: 8px;
+  border: none;
+  border-bottom: 1px solid #B0B5B3;
+  border-radius: 0;
+}
+
+.login-btn {
+  width: 7rem;
+  padding: 10px;
+  background: #9b3922;
+  color: #fffbfe;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.login-btn:hover {
+  background: #f2613f;
+}
 </style>
